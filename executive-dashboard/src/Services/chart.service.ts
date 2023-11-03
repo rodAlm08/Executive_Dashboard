f1d5ff03865c4983ebef7ceff65b90897896b7a9
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2023-11-03 15:34:22 +0000

    Service and components changed to ready data from a test.json file and display on the chart dynamically. It will also change the chart type once the user chose it on the drop box

--- executive-dashboard/src/Services/chart.service.ts
+++ executive-dashboard/src/Services/chart.service.ts
@@ -1,126 +1,185 @@
 import { Injectable } from '@angular/core';
-import { Chart , registerables} from 'chart.js';
+import { Chart, registerables } from 'chart.js';
+import { HttpClient } from '@angular/common/http';
+import { ChartType } from 'chart.js';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-Chart.register(...registerables)
+Chart.register(...registerables);
+
+interface CanvasWithChart extends HTMLCanvasElement {
+  'data-chart'?: Chart;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChartService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  single(
-    graphTitle: string,
-    key: string,
-    labels: any,
-    data: any,
-    context: string,
-    charttype: any
-  ) {
-    var chart = new Chart(context, {
-      type: charttype,
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            backgroundColor: [
-              'rgb(255,99,60)',
-              'rgb(54,162,235)',
-              'rgb(255,99,132)',
-              'rgb(255,205,86)',
-            ],
-            data: data,
-          },
-        ],
-      },
-      options: {},
-    });
+  fetchData(): Observable<any> {
+    return this.http.get('assets/data.json');
   }
 
-  double(
-    graphTitle: string,
-    primaryDatasetkey: string,
-    secondaryDatasetkey: string,
-    labels: any,
-    primaryDataset: any,
-    secondaryDataset: any,
-    context: string,
-    charttype: any
-  ) {
-    var chart = new Chart(context, {
-      type: charttype,
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            labels: primaryDatasetkey,
-            backgroundColor: ['rgb(31,97,141)'],
-            data: primaryDataset,
-          },
-          {
-            labels: secondaryDatasetkey,
-            backgroundColor: ['rgb(255,99,60)'],
-            data: secondaryDataset
-          }],
-      },
-      options: {
-        plugins:{
-          title:{
-            display:true,
-            text:graphTitle
-          },
-        },
-        responsive:true,
-        scales:{
-          x:{
-            stacked:true,
-          },
-          y:{
-            stacked:true
-          }
-        }
-      },
-    });
-  }
+  generateChart(chartType: string, labels: string[], data: number[], canvasId: string): void {
+    const canvas: CanvasWithChart = document.getElementById(canvasId) as CanvasWithChart;
 
-  scatter(
-    graphTitle: string,
-    labels: any,
-    data: any[],
-    context: string
-) {
-    var chart = new Chart(context, {
-        type: 'scatter',
+    if (!canvas) {
+        console.error(`Canvas element with ID '${canvasId}' not found.`);
+        return;
+    }
+
+    // Check if the canvas already has a chart instance
+    if (canvas['data-chart']) {
+        canvas['data-chart'].destroy(); // Destroy the existing chart instance
+    }
+
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+        console.error('Canvas context not available.');
+        return;
+    }
+
+    const newChart = new Chart(ctx, {
+        type: chartType as ChartType,
         data: {
             labels: labels,
             datasets: [{
-                label: graphTitle,
                 data: data,
-                backgroundColor: 'rgba(255, 99, 132, 1)', // Color of the scatter points
-                borderColor: 'rgba(255, 99, 132, 1)', // Color of the scatter point borders
-                borderWidth: 1, // Border width of the scatter points
-                pointRadius: 5, // Radius of the scatter points
+                backgroundColor: this.getRandomColors(data.length),
+                borderColor: this.getRandomColors(data.length),
+                borderWidth: 1
             }]
-        },
-        options: {
-            plugins: {
-                title: {
-                    display: true,
-                    text: graphTitle
-                }
-            },
-            scales: {
-                x: {
-                    type: 'linear', // 'linear' for numeric values, 'time' for time series data
-                    position: 'bottom'
-                },
-                y: {
-                    type: 'linear',
-                    position: 'left'
-                }
-            }
         }
     });
+
+    // Store the new chart instance in the canvas element for future reference
+    canvas['data-chart'] = newChart;
 }
 
+  private getRandomColors(count: number): string[] {
+    const colors: string[] = [];
+    for (let i = 0; i < count; i++) {
+      const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, 0.7)`;
+      colors.push(color);
+    }
+    return colors;
+  }
+
+  //   single(
+  //     graphTitle: string,
+  //     key: string,
+  //     labels: any,
+  //     data: any,
+  //     context: string,
+  //     charttype: any
+  //   ) {
+  //     var chart = new Chart(context, {
+  //       type: charttype,
+  //       data: {
+  //         labels: labels,
+  //         datasets: [
+  //           {
+  //             backgroundColor: [
+  //               'rgb(255,99,60)',
+  //               'rgb(54,162,235)',
+  //               'rgb(255,99,132)',
+  //               'rgb(255,205,86)',
+  //             ],
+  //             data: data,
+  //           },
+  //         ],
+  //       },
+  //       options: {},
+  //     });
+  //   }
+
+  //   double(
+  //     graphTitle: string,
+  //     primaryDatasetkey: string,
+  //     secondaryDatasetkey: string,
+  //     labels: any,
+  //     primaryDataset: any,
+  //     secondaryDataset: any,
+  //     context: string,
+  //     charttype: any
+  //   ) {
+  //     var chart = new Chart(context, {
+  //       type: charttype,
+  //       data: {
+  //         labels: labels,
+  //         datasets: [
+  //           {
+  //             labels: primaryDatasetkey,
+  //             backgroundColor: ['rgb(31,97,141)'],
+  //             data: primaryDataset,
+  //           },
+  //           {
+  //             labels: secondaryDatasetkey,
+  //             backgroundColor: ['rgb(255,99,60)'],
+  //             data: secondaryDataset
+  //           }],
+  //       },
+  //       options: {
+  //         plugins:{
+  //           title:{
+  //             display:true,
+  //             text:graphTitle
+  //           },
+  //         },
+  //         responsive:true,
+  //         scales:{
+  //           x:{
+  //             stacked:true,
+  //           },
+  //           y:{
+  //             stacked:true
+  //           }
+  //         }
+  //       },
+  //     });
+  //   }
+
+  //   scatter(
+  //     graphTitle: string,
+  //     labels: any,
+  //     data: any[],
+  //     context: string
+  // ) {
+  //     var chart = new Chart(context, {
+  //         type: 'scatter',
+  //         data: {
+  //             labels: labels,
+  //             datasets: [{
+  //                 label: graphTitle,
+  //                 data: data,
+  //                 backgroundColor: 'rgba(255, 99, 132, 1)', // Color of the scatter points
+  //                 borderColor: 'rgba(255, 99, 132, 1)', // Color of the scatter point borders
+  //                 borderWidth: 1, // Border width of the scatter points
+  //                 pointRadius: 5, // Radius of the scatter points
+  //             }]
+  //         },
+  //         options: {
+  //             plugins: {
+  //                 title: {
+  //                     display: true,
+  //                     text: graphTitle
+  //                 }
+  //             },
+  //             scales: {
+  //                 x: {
+  //                     type: 'linear', // 'linear' for numeric values, 'time' for time series data
+  //                     position: 'bottom'
+  //                 },
+  //                 y: {
+  //                     type: 'linear',
+  //                     position: 'left'
+  //                 }
+  //             }
+  //         }
+  //     });
+  // }
 }
--- executive-dashboard/src/app/app.component.ts
+++ executive-dashboard/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartService } from 'src/Services/chart.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -8,52 +11,81 @@ import { ChartService } from 'src/Services/chart.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  labelList: string[] = ['Car', 'Fruit', 'Chair', 'Table'];
-  data: number[] = [5, 2, 10, 3];
-  sdata: number[] = [1, 2, 3, 5];
-  chartName = 'Data Charts';
+  selectedChartType: string = '';
+  canvasId: string = 'chartCanvas';
+  chartData$: Observable<any> = this.chartService.fetchData();
+  chartData: any = {}; // Initialize an empty object
 
-  constructor(private chart: ChartService) {}
+
+  constructor(
+    private chartService: ChartService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.chart.single(
-      this.chartName,
-      'key',
-      this.labelList,
-      this.data,
-      'line-chart',
-      'line'
+    this.selectedChartType = 'bar'; // Set a default chart type
+    this.chartData$ = this.chartService.fetchData();
+    this.chartData$.subscribe(data => {
+      this.chartData = data;
+      this.renderChart();
+      this.cdr.detectChanges(); // Manually trigger change detection
+    });
+  }
+
+  onChartTypeChange(): void {
+    console.log('Selected chart type:', this.selectedChartType);
+    this.fetchDataAndRenderChart(this.selectedChartType);
+  }
+
+  private fetchDataAndRenderChart(chartType: string): void {
+    this.chartService.fetchData().subscribe(
+      (data) => {
+        this.chartData = data;
+        this.selectedChartType = chartType;
+        this.renderChart();
+        this.cdr.detectChanges(); // Manually trigger change detection
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
     );
+  }
 
-    this.chart.single(
-      this.chartName,
-      'key',
-      this.labelList,
-      this.data,
-      'doughnut-chart',
-      'doughnut'
-    );
-
-
-    this.chart.double(
-      this.chartName,
-      'key 1',
-      'key 2',
-      this.labelList,
-      this.data,
-      this.sdata,
-      'bar-chart',
-      'bar'
-    );
-
-    this.chart.scatter(
-      this.chartName, // graphTitle: string
-      this.labelList, // array of labels for the X-axis
-      this.data.map((value, index) => ({ x: value, y: this.sdata[index] })), // combining data and sdata for scatter points
-      'scatter-chart' // context: string
-  );
-  
-  
-  
+  private renderChart(): void {
+    const labels: string[] = this.chartData.labels;
+    const data: number[] = this.chartData.data;
+    this.chartService.generateChart(this.selectedChartType, labels, data, this.canvasId);
   }
 }
+
+  //   this.chart.single(
+  //     this.chartName,
+  //     'key',
+  //     this.labelList,
+  //     this.data,
+  //     'doughnut-chart',
+  //     'doughnut'
+  //   );
+
+
+  //   this.chart.double(
+  //     this.chartName,
+  //     'key 1',
+  //     'key 2',
+  //     this.labelList,
+  //     this.data,
+  //     this.sdata,
+  //     'bar-chart',
+  //     'bar'
+  //   );
+
+  //   this.chart.scatter(
+  //     this.chartName, // graphTitle: string
+  //     this.labelList, // array of labels for the X-axis
+  //     this.data.map((value, index) => ({ x: value, y: this.sdata[index] })), // combining data and sdata for scatter points
+  //     'scatter-chart' // context: string
+  // );
+  
+  
+  
+  // }
